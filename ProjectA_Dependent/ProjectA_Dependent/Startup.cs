d4b7b7f01448c8e3319b3fa46fb9a008d1def797
Prateek Varshney <prateek.varshney@nagarro.com>
2017-04-07 13:10:51 +0530

    Initial Commit

---
+++ ProjectA_Dependent/ProjectA_Dependent/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectA_Dependent.Startup))]
+namespace ProjectA_Dependent
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
